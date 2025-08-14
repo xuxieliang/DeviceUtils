2bd4c57d2b51ffd33bd48c2584c9735dd57dca55
kevin <xuxieliang@yicai.com>
2025-08-14 16:07:48 +0800

    更新内容说明

--- Package.swift
+++ Package.swift
@@ -17,21 +17,23 @@ let package = Package(
     targets: [
         // Objective-C 部分
         .target(
-            name: "XLPageController",
-            dependencies: ["XLMenuView"],
-            path: "Sources/XLPageController",
-            publicHeadersPath: ".", // 让 OC 的头文件可以暴露出去
-            cSettings: [
-                .headerSearchPath(".")
-            ]
-        ),
-        .target(
             name: "XLMenuView",
             path: "Sources/XLMenuView",
             publicHeadersPath: ".", // 让 OC 的头文件可以暴露出去
             cSettings: [
                 .headerSearchPath(".")
             ]
+        ),
+        .target(
+            name: "XLPageController",
+            dependencies: ["XLMenuView"],
+            path: "Sources/XLPageController",
+            publicHeadersPath: ".", // 让 OC 的头文件可以暴露出去
+            cSettings: [
+                .headerSearchPath("."),
+                .headerSearchPath("../XLMenuView") // 手动加上 XLMenuView 的头文件路径
+            ]
         )
+        
     ]
 )
